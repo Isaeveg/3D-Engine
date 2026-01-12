8715c8f69f4a09b504fbce9182334ff55b939cd3
Isaiev Yehor <isaev.egor.08@gmail.com>
2026-01-12 18:56:34 +0100

    Pepe

--- src/Engine.h
+++ src/Engine.h
@@ -1,86 +1,69 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <vector>
 #include <string>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <iostream>
-#include <string>
-
 #include "Camera.h"
-#include "Observer.h" // <--- Добавлено: нужно для использования класса Observer
 
-enum ProjectionType
-{
-    ORTHOGRAPHIC,
-    PERSPECTIVE
+enum ShapeType { CUBE, PYRAMID, SPHERE, TEAPOT, TORUS };
+
+struct GameObject {
+    glm::vec3 pos;
+    glm::vec3 rot;
+    float scale;
+    glm::vec3 color;
+    ShapeType type;
+    bool useTexture;
 };
 
-class Engine
-{
+class Engine {
 private:
-    int windowWidth;
-    int windowHeight;
-    std::string windowTitle;
-    int windowHandle;
-    int fps;
+    int windowWidth, windowHeight;
+    int targetFPS;
+    bool keys[256], specialKeys[256];
+    Camera* camera;
+    std::vector<GameObject> objects;
+    int selectedIdx;
+    
+    // ПЕРЕМЕННЫЕ ДЛЯ СВЕТА И ТЕНИ
+    bool lightEnabled;
+    bool smoothShading;
+    GLuint crateTexture;
 
-    ProjectionType currentProjection;
-    float clearColor[4];
-
-    Camera *camera;
-    Observer observer;          // <--- Добавлено: поле observer, которое используется в cpp
-    float rotationAngle = 0.0f; // <--- Добавлено: переменная для вращения объектов
-
-    // --- Управление вводом ---
-    bool keys[1024]; // Массив состояний клавиш (нажата/отпущена)
-
-    // Для мыши
-    bool firstMouse;
-    float lastX, lastY;
-
-    static Engine *instance;
+    static Engine* instance;
 
     void render();
     void update(int value);
     void reshape(int w, int h);
-
-    // Обработчики событий
-    void keyboard(unsigned char key, int x, int y);
-    void keyboardUp(unsigned char key, int x, int y); // Когда клавишу отпустили
-    void mouseMove(int x, int y);                     // Движение мыши
-
-    // Обработка логики движения (вызывается каждый кадр)
     void processInput();
+    void loadTexture();
+    void drawInterface();
 
-    // Статические обертки
-    static void renderWrapper();
-    static void updateWrapper(int value);
-    static void reshapeWrapper(int w, int h);
-    static void keyboardWrapper(unsigned char key, int x, int y);
-    static void keyboardUpWrapper(unsigned char key, int x, int y);
-    static void mouseMoveWrapper(int x, int y);
+    void keyboard(unsigned char key, int x, int y);
+    void keyboardUp(unsigned char key, int x, int y);
+    void specialFunc(int key, int x, int y);
+    void specialUpFunc(int key, int x, int y);
+    void mouseMove(int x, int y);
 
 public:
     Engine();
     ~Engine();
+    static Engine& getInstance() { static Engine inst; return inst; }
 
-    static Engine &getInstance()
-    {
-        static Engine instance;
-        return instance;
-    }
-
-    void init(int argc, char **argv, int width, int height, const std::string &title);
+    void init(int argc, char** argv, int w, int h, const std::string& title);
     void run();
-    void setClearColor(float r, float g, float b, float a);
-    void setFPS(int value);
-    void setProjection(ProjectionType type);
+    void spawnObject(ShapeType type);
 
-    void drawWireCube(double size);
-    void drawWireTeapot(double size);
+    static void renderWrapper();
+    static void updateWrapper(int v);
+    static void reshapeWrapper(int w, int h);
+    static void kbWrapper(unsigned char k, int x, int y);
+    static void kbUpWrapper(unsigned char k, int x, int y);
+    static void specWrapper(int k, int x, int y);
+    static void specUpWrapper(int k, int x, int y);
+    static void mouseWrapper(int x, int y);
 };
 
-#endif // <--- Добавлено: закрывающий endif
+#endif
--- src/Observer.cpp
+++ src/Observer.cpp
@@ -1,26 +1,7 @@
 #include "Observer.h"
-#include <cmath>
 
-Observer::Observer()
-{
-    center = glm::vec3(0.0f, 0.0f, 0.0f);
-    up = glm::vec3(0.0f, 1.0f, 0.0f);
-    radius = 15.0f;
-    theta = 0.7f;
-    phi = 0.5f;
-    updatePosition();
-}
+Observer::Observer() : eye(0,2,15), center(0,0,0), up(0,1,0) {}
 
-void Observer::updatePosition()
-{
-    eye.x = center.x + radius * cos(phi) * sin(theta);
-    eye.y = center.y + radius * sin(phi);
-    eye.z = center.z + radius * cos(phi) * cos(theta);
-}
-
-void Observer::apply()
-{
-    gluLookAt(eye.x, eye.y, eye.z,
-              center.x, center.y, center.z,
-              up.x, up.y, up.z);
+void Observer::apply() {
+    gluLookAt(eye.x, eye.y, eye.z, center.x, center.y, center.z, up.x, up.y, up.z);
 }
